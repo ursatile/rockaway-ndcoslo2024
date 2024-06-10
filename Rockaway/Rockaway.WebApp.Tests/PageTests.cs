241519545d057edc171c093a62cbc6704ebb0e9f
Dylan Beattie <dylan@dylanbeattie.net>
2024-06-10 10:54:42 +0200

    test for privacy page

--- Rockaway/Rockaway.WebApp.Tests/PageTests.cs
+++ Rockaway/Rockaway.WebApp.Tests/PageTests.cs
@@ -3,12 +3,24 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Rockaway.WebApp.Tests.Pages;
-public class PageTests {
+public class PageTests
+{
 	[Fact]
-	public async Task Index_Page_Returns_Success() {
+	public async Task Index_Page_Returns_Success()
+	{
 		await using var factory = new WebApplicationFactory<Program>();
 		using var client = factory.CreateClient();
 		using var response = await client.GetAsync("/");
 		response.EnsureSuccessStatusCode();
 	}
+
+	[Fact]
+	public async Task Privacy_Page_Returns_Success()
+	{
+		await using var factory = new WebApplicationFactory<Program>();
+		using var client = factory.CreateClient();
+		using var response = await client.GetAsync("/privacy");
+		response.EnsureSuccessStatusCode();
+	}
+
 }
