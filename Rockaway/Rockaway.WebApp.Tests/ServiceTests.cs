179aa23e80016d17fdda0bf003b72b1520af7c6a
Dylan Beattie <dylan@dylanbeattie.net>
2024-06-10 12:12:57 +0200

    Working teststatusreporter

--- Rockaway/Rockaway.WebApp.Tests/ServiceTests.cs
+++ Rockaway/Rockaway.WebApp.Tests/ServiceTests.cs
@@ -6,6 +6,15 @@ using Xunit.Abstractions;
 
 namespace Rockaway.WebApp.Tests.Pages;
 
+public class TestStatusReporter : IStatusReporter {
+	public static ServerStatus TestStatus = new() {
+		Assembly = "Rockaway.WebApp",
+		Modified = "2021-01-01T00:00:00Z",
+		Hostname = "TEST-HOSTNAME",
+		DateTime = "2021-01-01T02:03:04Z"
+	};
+	public ServerStatus GetStatus() => TestStatus;
+}
 
 public class ServiceTests(ITestOutputHelper helper) {
 	private static readonly JsonSerializerOptions jsonSerializerOptions
@@ -21,12 +30,17 @@ public class ServiceTests(ITestOutputHelper helper) {
 
 	[Fact]
 	public async Task StatusEndpoint_Returns_Correct_Details() {
-		await using var factory = new WebApplicationFactory<Program>();
+		var factory = new WebApplicationFactory<Program>()
+			.WithWebHostBuilder(builder => builder.ConfigureServices(services => {
+				services.AddSingleton<IStatusReporter>(new TestStatusReporter());
+			}));
 		using var client = factory.CreateClient();
 		var response = await client.GetStringAsync("/status");
 		helper.WriteLine(response);
 		var status = JsonSerializer.Deserialize<ServerStatus>(response, jsonSerializerOptions)!;
-		// status.Assembly.ShouldBe("Rockaway.WebApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
-		status.Hostname.ShouldBe("DYLAN-FRAMEWORK");
+		status.Hostname.ShouldBe(TestStatusReporter.TestStatus.Hostname);
+		status.DateTime.ShouldBe(TestStatusReporter.TestStatus.DateTime);
+		status.Modified.ShouldBe(TestStatusReporter.TestStatus.Modified);
+		status.Assembly.ShouldBe(TestStatusReporter.TestStatus.Assembly);
 	}
 }
