6f435039039d240bfcff8741852a72e90f79d178
Dylan Beattie <dylan@dylanbeattie.net>
2024-06-11 10:21:48 +0200

    Artist gallery including images

--- Rockaway/Rockaway.WebApp/Pages/Artists.cshtml.cs
+++ Rockaway/Rockaway.WebApp/Pages/Artists.cshtml.cs
@@ -1,13 +1,16 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Rockaway.WebApp.Data;
 using Rockaway.WebApp.Data.Entities;
+using Rockaway.WebApp.Models;
 
 namespace Rockaway.WebApp.Pages;
 
 public class ArtistsModel(RockawayDbContext db) : PageModel {
-	public IEnumerable<Artist> Artists = default!;
+	public IEnumerable<ArtistViewData> Artists = default!;
 
 	public void OnGet() {
-		Artists = db.Artists.OrderBy(a => a.Name);
+		Artists = db.Artists
+			.OrderBy(a => a.Name)
+			.Select(a => new ArtistViewData(a));
 	}
 }
